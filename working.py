7dea9d29ed983d1efc5a90ab5a76b52c037d0c3d
NIROB SHEIKH <154347282+NIROB-909@users.noreply.github.com>
2024-01-12 00:26:46 +0600

    Update working.py

--- working.py
+++ working.py
@@ -1,7 +1,8 @@
 #খানকির পোলা পারলে ফাইল চুরি কর 
 #তর পাপা NIROB SHEIKH মাদারবোর্ড
 #তর টিম কে আন্তর্জাতিক ভাবে চুদি 
+#Hello Hacker,s 0.2 TEAM
 
 import os
-import.working.py
+import working.py
 working.py.loging()
